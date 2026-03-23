6843b7e2729b328708498dd9d0ef13c8b3a9ca24
Khush81699 <khushboosgupta864@gmail.com>
2026-03-23 23:53:02 +0530

    Day 20 - Views and data security

---
+++ day20_views_security.sql
@@ -0,0 +1,54 @@
+USE company_db1;
+
+DROP TABLE IF EXISTS employees_d20;
+
+CREATE TABLE employees_d20 (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO employees_d20 VALUES
+(1,'Khushboo','IT',60000),
+(2,'Shivam','HR',45000),
+(3,'Kumkum','IT',70000),
+(4,'Rahul','Finance',50000),
+(5,'Aman','IT',55000);
+
+SELECT * FROM employees_d20;
+
+
+-- View for HR (hide salary)
+CREATE VIEW employee_public_view AS
+SELECT id, name, department
+FROM employees_d20;
+
+SELECT * FROM employee_public_view;
+
+
+-- View for managers (high salary employees)
+CREATE VIEW high_salary_view AS
+SELECT name, department, salary
+FROM employees_d20
+WHERE salary > 55000;
+
+SELECT * FROM high_salary_view;
+
+
+-- View with restricted department data
+CREATE VIEW it_department_view AS
+SELECT name, salary
+FROM employees_d20
+WHERE department = 'IT';
+
+SELECT * FROM it_department_view;
+
+
+-- Show all views
+SHOW FULL TABLES
+WHERE TABLE_TYPE = 'VIEW';
+
+
+-- Drop view example
+DROP VIEW IF EXISTS high_salary_view;
